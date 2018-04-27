fad80a11ed672e232dfc7e8ae77e0e33ed6368c3
Edilio <edilio@degconnect.com>
2018-04-27 11:52:21 -0400

    only do the middleware for pagination on urls starting with BOOTSTRAP_PAGINATION_URL_STARTINGS settings

--- pagination_bootstrap/middleware.py
+++ pagination_bootstrap/middleware.py
@@ -1,8 +1,15 @@
+from django.conf import settings
+
+BOOTSTRAP_PAGINATION_URL_STARTINGS = getattr(settings, 'BOOTSTRAP_PAGINATION_URL_STARTINGS', None)
+if BOOTSTRAP_PAGINATION_URL_STARTINGS:
+    BOOTSTRAP_PAGINATION_URL_STARTINGS = BOOTSTRAP_PAGINATION_URL_STARTINGS.split(',')
+
 try:
     from django.utils.deprecation import MiddlewareMixin
     object = MiddlewareMixin
 except:
     pass
+
 
 def get_page(self):
     """
@@ -21,4 +28,13 @@ class PaginationMiddleware(object):
     it exists in either **GET** or **POST** portions of the request.
     """
     def process_request(self, request):
-        request.__class__.page = property(get_page)
+        if BOOTSTRAP_PAGINATION_URL_STARTINGS:
+            path = request.path
+            for start in BOOTSTRAP_PAGINATION_URL_STARTINGS:
+                if path.startswith(start):
+                    request.__class__.page = property(get_page)
+                    return
+        page = getattr(request.__class__, 'page', None)
+        if page is not None and 'property' in str(type(page)):
+            request.__class__.page = None
+
